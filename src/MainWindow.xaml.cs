4e6d1a077310edc65c244d676340f9ea0b1a8ccf
OblivCode <60195126+OblivCode@users.noreply.github.com>
2021-08-13 13:20:17 +0100

    Update MainWindow.xaml.cs

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -41,14 +41,10 @@ namespace app
         
 
         public async Task MainAsync()
-        {
-           
-            server = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
-            
+        {          
+            server = new TcpListener(IPAddress.Parse("127.0.0.1"), port);            
             server.Start();
             Console.WriteLine("Server has started on 127.0.0.1:800.\nWaiting for a connection...");
-
-            
 
             clientTimer = new Timer();
             clientTimer.Elapsed += ClientTimer_Elapsed;
@@ -114,10 +110,6 @@ namespace app
 
         private async void ClientTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            this.Dispatcher.Invoke(() =>
-            {
-                statusLabel.Content = clientList.Count;
-            });
             if (clientList.Count == 0)
             {
                 this.Dispatcher.Invoke(() =>
@@ -216,7 +208,7 @@ namespace app
 
             if (Regex.IsMatch(data, "^GET"))
             {
-                const string eol = "\r\n"; // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
+                const string eol = "\r\n"; // end-of-line marker
 
                 Byte[] response = Encoding.UTF8.GetBytes("HTTP/1.1 101 Switching Protocols" + eol
                     + "Connection: Upgrade" + eol
@@ -235,26 +227,18 @@ namespace app
             else
             {
                 bool fin = (bytes[0] & 0b10000000) != 0,
-                    mask = (bytes[1] & 0b10000000) != 0; // must be true, "All messages from the client to the server have this bit set"
+                    mask = (bytes[1] & 0b10000000) != 0; 
 
-                int opcode = bytes[0] & 0b00001111, // expecting 1 - text message
+                int opcode = bytes[0] & 0b00001111, 
                     msglen = bytes[1] - 128, // & 0111 1111
                     offset = 2;
 
                 if (msglen == 126)
                 {
-                    // was ToUInt16(bytes, offset) but the result is incorrect
                     msglen = BitConverter.ToUInt16(new byte[] { bytes[3], bytes[2] }, 0);
                     offset = 4;
                 }
-                else if (msglen == 127)
-                {
-                    Console.WriteLine("TODO: msglen == 127, needs qword to store msglen");
-                    // i don't really know the byte order, please edit this
-                    // msglen = BitConverter.ToUInt64(new byte[] { bytes[5], bytes[4], bytes[3], bytes[2], bytes[9], bytes[8], bytes[7], bytes[6] }, 0);
-                    // offset = 10;
-                }
-
+                
                 if (msglen == 0)
                     Console.WriteLine("msglen == 0");
                 else if (mask)
